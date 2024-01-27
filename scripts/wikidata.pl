acce9b2fe2bb9855a8a3aa00f9d2ae9bfe7d7698
Trond Trosterud <trond.trosterud@uit.no>
2024-01-27 15:26:59 +0200

    xml:lang info

--- scripts/wikidata.pl
+++ scripts/wikidata.pl
@@ -10,14 +10,14 @@ print STDOUT "<r>\n";
 while (<>) 
 {
 	chomp ;
-#	my ($lemma, $POS, $trans, $trans2, $trans3) = split /\t/ ;
+#	my ($item, $Fi, $Smn, $Se, $Sms, $Sma, $Smj, $Nb, $En, $Ru) = split /\t/ ;
 	my ($item, $Fi, $Smn, $Se, $Sms, $Sma, $Smj, $Nb, $En, $Ru) = split /\t/ ;
 	print STDOUT "   <e src=\"$item\">\n";
 	print STDOUT "      <lg>\n";
 	print STDOUT "         <l pos=\"N\" type=\"Prop\" sem_type=\"Plc\">$Smn</l>\n";
 	print STDOUT "      </lg>\n";
 	print STDOUT "      <mg>\n";
-	print STDOUT "         <tg>\n";
+	print STDOUT "         <tg xml:lang=\"fin\">\n";
 	print STDOUT "            <t pos=\"N\" type=\"Prop\">$Fi</t>\n";
 #	print STDOUT "            <t pos=\"$POS\">$trans2</t>\n";
 #	print STDOUT "            <t pos=\"$POS\">$trans3</t>\n";
